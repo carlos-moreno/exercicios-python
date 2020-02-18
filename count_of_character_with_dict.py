3dbeb33563d91b48bdfe832c0f2268c70a99422f
Carlos Moreno <omorenodovale@gmail.com>
2020-02-18 10:25:03 -0500

    Refactoring character count exercise with dict

--- count_of_character_with_dict.py
+++ count_of_character_with_dict.py
@@ -10,21 +10,10 @@ def count_character(s: str):
     {'a': 1, 'o': 1, 'p': 2, 'r': 1, 's': 2, 't': 1}
     """
 
-    character_sorted = sorted(s)
-    previous_character = character_sorted[0]
-    count = 1
-
     result = {}
 
-    for character in character_sorted[1:]:
-        if character == previous_character:
-            count += 1
-        else:
-            result[previous_character] = count
-            count = 1
-            previous_character = character
-
-    result[previous_character] = count
+    for character in s:
+        result[character] = result.get(character, 0) + 1
 
     return result
 
